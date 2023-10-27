b5df46b196e0a2965a0d200007fb6569b88ff416
danaeliu <danaeliu@tencent.com>
2023-10-27 17:45:55 +0800

    feat:get the size of class vptr

--- comm/test_class_polymorphism.h
+++ comm/test_class_polymorphism.h
@@ -2,16 +2,17 @@
 #define DE1DB72F_C7F9_4320_9529_CEA55161AD06
 #include "test.h"
 
-class Base {};
+class Base1 {};
+class Base2 {virtual void print(){};};
+class Driverd : public Base1 {};
 
 void test_class_polymorphism() {
-  Base b1;
-  std::cout << "sizeof(Base) = " << sizeof(Base) << std::endl;
+  Base1 b1;
+  std::cout << "sizeof(Base1) = " << sizeof(Base1) << std::endl;
   std::cout << "sizeof(b1) = " << sizeof(b1) << std::endl;
+  std::cout << "sizeof(Base2) = " << sizeof(Base2) << std::endl;
 }
 
 #endif /* DE1DB72F_C7F9_4320_9529_CEA55161AD06 */
-
-
 
 
